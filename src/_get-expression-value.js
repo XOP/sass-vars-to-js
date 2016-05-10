2af0b87e96fe5f6ba757ce5ca31dbcefffe13161
XOP <wdybih@gmail.com>
2016-05-10 16:41:10 +0200

    moved expression parser to src

---
+++ src/_get-expression-value.js
@@ -0,0 +1,62 @@
+import path from 'path';
+
+import is from '../node_modules/is';
+import sass from '../node_modules/node-sass';
+
+import message from './utils/_message.js';
+
+/**
+ * Resolve variable value
+ * By variable name and previous code
+ * @param varName
+ * @param scssExpression
+ */
+function getExpressionValue (varName, scssExpression) {
+    if (is.undef(scssExpression) || is.args.empty(arguments)) {
+        message('Error: Missing arguments');
+        return undefined;
+    }
+
+    if (!is.string(varName) || !is.string(scssExpression)) {
+        message('Error: Check arguments type');
+        return undefined;
+    }
+
+    if (!~scssExpression.indexOf('$')) {
+        message('Warning: Check scssExpression to contain valid code')
+    }
+
+    // print the interpolated value in comment string
+    // /*#{$varName}*/ --> /*varValue*/
+    const scssContent = `${scssExpression}/*#{$${varName}}*/`;
+
+    const sassResult = sass.renderSync({
+        data: scssContent
+    });
+
+    const cssResult = sassResult.css.toString();
+
+    return extractExpressionValue(cssResult);
+}
+
+/**
+ * Parse calculated variable value
+ * From CSS comment
+ * @param css
+ * @returns {*}
+ */
+function extractExpressionValue (css) {
+
+    // parse the comment string:
+    // /*varValue*/ --> varValue
+    const valueRegExp = /(?:\/\*)(.+)(?:\*\/)/;
+    const value = css.match(valueRegExp)[1];
+
+    if (!value) {
+        message('Warning: empty value');
+    }
+
+    return value;
+}
+
+export default getExpressionValue;
--- tests/node-sass-test.js
+++ tests/node-sass-test.js
@@ -1,50 +1,40 @@
 var TEST_NAME = 'node-sass';
 
 var test = require('tape');
-var sass = require('../node_modules/node-sass');
 
-/*
-    FIXME: move to dedicated file
- */
-function extractExpressionValue (css) {
-
-    // parse the comment string:
-    // /*varValue*/ --> varValue
-    var valueRegExp = /(?:\/\*)(.+)(?:\*\/)/;
-    var value = css.match(valueRegExp)[1];
-
-    return value;
-}
-
-/*
-    FIXME: move to dedicated file
- */
-function resolveExpressionValue (varName, scssString) {
-
-    // print the interpolated value in comment string
-    // /*#{$varName}*/ --> /*varValue*/
-    var scssContent = scssString + '' +
-        '/*' + '#{$' + varName + '}*/';
-
-    var sassResult = sass.renderSync({
-        data: scssContent
-    });
-
-    var cssResult = sassResult.css.toString();
-
-    return extractExpressionValue(cssResult);
-}
+var getExpressionValue = require('../dist/_get-expression-value.js');
 
 test(TEST_NAME, function (t) {
 
-    var data, result;
+    var data;
+
+    t.equal(
+        getExpressionValue(),
+        undefined,
+
+        'should return undefined if no arguments provided'
+    );
+
+    t.equal(
+        getExpressionValue('color'),
+        undefined,
+
+        'should return undefined if no expression provided'
+    );
 
     data = '' +
         '$color: #fce;' +
         '$color-dark: darken($color, 50%);';
 
     t.equal(
-        resolveExpressionValue('color-dark', data),
+        getExpressionValue(1000, data),
+        undefined,
+
+        'should return undefined if arguments types are invalid'
+    );
+
+    t.equal(
+        getExpressionValue('color-dark', data),
         '#cc0088',
 
         'should return sass function result'
@@ -56,14 +46,14 @@ test(TEST_NAME, function (t) {
         '$color-darker: darken($color, 75%);';
 
     t.equal(
-        resolveExpressionValue('color-dark', data),
+        getExpressionValue('color-dark', data),
         '#cc0088',
 
         'should return sass function result'
     );
 
     t.equal(
-        resolveExpressionValue('color-darker', data),
+        getExpressionValue('color-darker', data),
         '#4d0033',
 
         'should return sass function result'
